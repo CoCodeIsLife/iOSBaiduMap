82eeb10cfb97b3ea4e32eafb0c5d37d8ef79dc6f
CoCodeIsLife <lliuyanqil>
2016-06-07 19:58:33 +0800

    添加大头针

--- BaiduMapIOS/ViewController.m
+++ BaiduMapIOS/ViewController.m
@@ -21,14 +21,11 @@
     [super viewDidLoad];
     [self creatArrays];
     [self creatMapView];
-    
-    
-    
-    
-    
+//    [self daTouZhenBiaoZhu];
     
     
 }
+//创建数组
 -(void)creatArrays
 {
     _arrayBtnName = @[@"普通",@"卫星",@"热力",@"交通"];
@@ -37,8 +34,8 @@
 {
     //地图
     _mapView = [[BMKMapView alloc]initWithFrame:CGRectMake(0, 80, self.view.frame.size.width, self.view.frame.size.height - 80)];
-//    self.view = _mapView;
-    [self.view addSubview:_mapView];
+    self.view = _mapView;
+//    [self.view addSubview:_mapView];
     for (int i = 0;i<4 ;i++)
     {
         UIButton *buttonType = [UIButton buttonWithType:UIButtonTypeCustom];
@@ -49,9 +46,17 @@
         [buttonType addTarget:self action:@selector(mapTypeClick:) forControlEvents:UIControlEventTouchUpInside];
         [self.view addSubview:buttonType];
     }
-
+    
+    //定位按钮
+    UIButton *buttonL = [UIButton buttonWithType:UIButtonTypeCustom];
+    buttonL.frame = CGRectMake(10,self.view.frame.size.height, 15,15);
+    buttonL.backgroundColor = [UIColor lightGrayColor];
+    [buttonL addTarget:self action:@selector(myPlace) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:buttonL];
+    
     
 }
+//点击选择不同的地图类型
 -(void)mapTypeClick:(UIButton *)sender
 {
     //关闭百度城市热力图图层（百度自有数据）
@@ -86,6 +91,40 @@
             break;
     }
 }
+-(void)myPlace
+{
+    //大头针标注
+    BMKPointAnnotation* annotation = [[BMKPointAnnotation alloc]init];
+    CLLocationCoordinate2D coor;
+    coor.latitude = 39.915;
+    coor.longitude = 116.404;
+    annotation.coordinate = coor;
+    annotation.title = @"这里是北京";
+    [_mapView addAnnotation:annotation];
+}
+-(void)daTouZhenBiaoZhu
+{
+    //大头针标注
+    BMKPointAnnotation* annotation = [[BMKPointAnnotation alloc]init];
+    CLLocationCoordinate2D coor;
+    coor.latitude = 39.915;
+    coor.longitude = 116.404;
+    annotation.coordinate = coor;
+    annotation.title = @"这里是北京";
+    [_mapView addAnnotation:annotation];
+}
+//大头针
+//-(BMKAnnotationView *)mapView:(BMKMapView *)mapView viewForAnnotation:(id<BMKAnnotation>)annotation
+//{
+//    if([annotation isKindOfClass:[BMKAnnotationView class]])
+//    {
+//        BMKPinAnnotationView *annotationView = [[BMKPinAnnotationView alloc]initWithAnnotation:annotation reuseIdentifier:@"myAnnotation"];
+//        annotationView.pinColor = BMKPinAnnotationColorRed;
+//        annotationView.animatesDrop = YES;
+//        return annotationView;
+//    }
+//    return nil;
+//}
 -(void)viewWillAppear:(BOOL)animated
 {
     [_mapView viewWillAppear];
